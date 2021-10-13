12f1828e2ebb47b001c137935c1a40531d691c62
Ryan Dimick <rdimick@smatechnologies.com>
2021-10-13 17:10:30 -0500

    Initialize repository

---
+++ docusaurus.config.js
@@ -0,0 +1,50 @@
+/** @type {import('@docusaurus/types').DocusaurusConfig} */
+module.exports = {
+  title: 'SMA Technologies Help',
+  tagline: 'OpCon Basic Training',
+  url: 'https://help.smatechnologies.com',
+  baseUrl: '/training/opcon-basic/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'smatechnologies',
+  projectName: 'opcon-basic-training',
+  themeConfig: {
+    navbar: {
+      title: 'Help',
+      logo: {
+        alt: 'SMA Technologies Help Logo',
+        src: 'img/logo.svg',
+        href: 'https://help.smatechnologies.com',
+      },
+    },
+    footer: {
+      style: 'dark',
+      copyright: `Copyright © ${new Date().getFullYear()} SMA Technologies.`,
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          routeBasePath: '/',
+          sidebarPath: require.resolve('./sidebars.js'),
+          editUrl:
+            'https://github.com/smatechnologies/opcon-basic-training/blob/develop',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+  plugins: [
+    [
+      require.resolve('@cmfcmf/docusaurus-search-local'), 
+      {
+        docsRouteBasePath: '/', 
+      }
+    ],
+  ],
+};
